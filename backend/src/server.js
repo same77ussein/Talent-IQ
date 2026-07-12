2253585edbf71a952d601912ee196a28965924ec
sameh hussein <samehhussein@ivoiceup.com>
2026-07-12 21:47:52 +0300

    feat: initialize frontend with React, Vite, and Clerk integration
    - Added SVG icons for various services (Bluesky, Discord, Documentation, GitHub, Social, X).
    - Created main application component with sign-in and sign-up functionality using Clerk.
    - Included assets: hero image, React logo, and Vite logo.
    - Set up TypeScript configuration for app and node environments.
    - Configured Vite for React application.
    - Created package.json with build and start scripts for backend and frontend.

---
+++ backend/src/server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import path from "path";
+import cors from "cors";
+import inngest from "inngest/express";
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import { functions } from "./lib/inggest.js";
+const app = express();
+
+const __dirname = path.resolve();
+
+// middleware
+app.use(express.json());
+
+// Enable CORS for the specified client URL and allow credentials
+// Credentials are necessary for cookies and authentication headers to be sent in cross-origin requests
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "Server is running" });
+});
+
+// Serve static files and handle routing for production
+if (ENV.NODE_ENV === "production") {
+  console.log("Running in production mode");
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("/{*any}/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, async () => {
+      console.log(`Server is running on port ${ENV.PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Error starting the server", error);
+    process.exit(1); // 0 means success, 1 means failure
+  }
+};
+
+startServer();
